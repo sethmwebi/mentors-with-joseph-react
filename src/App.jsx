55be59ecde4d2ac294aa150b64449989215a1f04
Seth Mwebi <sethmwebi27@gmail.com>
2025-01-22 18:34:53 +0300

    first look at react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
-import "./App.css";
+// import "./App.css";
+// import Profile from "./Profile";
+//
+// function Gallery() {
+//   return (
+//     <div>
+//       <Profile />
+//       <Profile />
+//       <Profile />
+//     </div>
+//   );
+// }
+//
+// export default Gallery;
+//
 
-function App() {
-  return <h1>Hello World!</h1>;
+// export default function TodoList() {
+//   return (
+//     <>
+//       <h1>Hedy Lamarr's Todos</h1>
+//       <img
+//         src="https://i.imgur.com/yXOvdOSs.jpg"
+//         alt="Hedy Lamarr"
+//         class="photo"
+//       />
+//       <ul>
+//         <li>Invent new traffic lights</li>
+//         <li>Rehearse a movie scene</li>
+//         <li>Improve spectrum technology</li>
+//       </ul>
+//     </>
+//   );
+// }
+const podcaster = {
+  name: "Lex Fridman",
+  theme: {
+    backgroundColor: "black",
+    color: "green",
+  },
+};
+
+export default function TodoList() {
+  return (
+    <div
+      style={{
+        backgroundColor: podcaster.theme.backgroundColor,
+        color: podcaster.theme.color,
+      }}
+    >
+      <h1>{podcaster.name}</h1>
+    </div>
+  );
 }
-
-export default App;
---
+++ src/Profile.jsx
@@ -0,0 +1,11 @@
+function Profile() {
+  return (
+    <img
+      src="https://images.pexels.com/photos/29726820/pexels-photo-29726820/free-photo-of-woman-enjoying-nature-in-ha-giang-forest.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      alt="girl wearing red sweater"
+      className="image"
+    />
+  );
+}
+
+export default Profile;
